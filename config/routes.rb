958d2311769dbfabc04b61688f9443b35725738a
martinBaillie42 <martin@martinbaillie.net>
2014-11-10 00:58:45 +0000

    devise and omniauth seems to work. Sort of.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'static_pages/home'
+  # get 'users/auth/:provider/callback', to: 'sessions#create'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # get 'static_pages/home'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
