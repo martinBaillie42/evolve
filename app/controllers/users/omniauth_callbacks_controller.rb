5d151627f13a4b71d22754cc519687ad3606351e
martinBaillie42 <martin@martinbaillie.net>
2014-11-10 01:24:32 +0000

    Added TODO

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # TODO amend using http://richonrails.com/articles/google-authentication-in-ruby-on-rails
   def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ActiveRecord::Base
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
-        user = User.create(name: data["name"],
-           email: data["email"],
-           password: Devise.friendly_token[0,20]
-        )
+      user = User.create(name: data["name"],
+        email: data["email"],
+        password: Devise.friendly_token[0,20]
+      )
     end
     user
   end
