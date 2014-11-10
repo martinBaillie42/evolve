85f6f76a16079e772ff5bf0c78c76382ddf006ab
martinBaillie42 <martin@martinbaillie.net>
2014-11-10 22:15:51 +0000

    home and sessions contrller removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'home/show'
-
   # get 'users/auth/:provider/callback', to: 'sessions#create'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # get 'static_pages/home'
