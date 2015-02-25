e6e97c0d16afb9c763f45719f497ab75fc782c58
martinBaillie42 <martin@martinbaillie.net>
2015-02-25 14:46:33 +0000

    Current element info now going to menu.

--- app/assets/javascripts/experiments_vanilla.js
+++ app/assets/javascripts/experiments_vanilla.js
@@ -12,10 +12,29 @@ var emvt = emvt || {};
 
         // TODO Is typeof function reliable?
         emvt.initialiseMenu = function () {
-                //emvt.clickedElement.wrap('<div class="dropdown">');
-                //debugger;
-
-            };
+            //console.log(emvt.clickedElement);
+            var tagName = emvt.clickedElement.prop('tagName').toLowerCase();
+            var id = emvt.clickedElement.attr('id');
+            var classes = emvt.clickedElement.attr('class');
+            var parent = emvt.clickedElement.parent();
+            var firstChild = emvt.clickedElement.children()[0]; // this might play up on only one return
+            var text = emvt.clickedElement.contents().filter(function() {
+                    // http://stackoverflow.com/questions/5913031/jquery-get-text-but-not-the-text-in-span
+                    return this.nodeType == 3;
+                }).text();
+            console.log('id', id);
+            console.log('classes', classes);
+            console.log('parent', parent);
+            console.log('firstChild', firstChild);
+            console.log('text', text);
+            $('span', '[data-emvt="menu-tagname"]').text(tagName);
+            $('span', '[data-emvt="menu-id"]').text(id);
+            $('span', '[data-emvt="menu-classes"]').text(classes);
+            $('span', '[data-emvt="menu-parent"]').text(parent.prop('tagName').toLowerCase() + parent.attr('id') + parent.attr('class'));
+            $('span', '[data-emvt="menu-firstchild"]').text(firstChild);
+            $('span', '[data-emvt="menu-text"]').text(text);
+            //debugger;
+        };
 
         // Change cursor to pointer when entering iframe, back to default on way out
         emvt.targetPage.find('body').on('mouseenter', function() {
@@ -27,8 +46,11 @@ var emvt = emvt || {};
         // Highlight and add a class to the current hover over element
         $('*', emvt.targetPage).on('mouseenter', function() {
             if (emvt.highlightOn){
-                $('.emvt_red', emvt.targetPage).css('outline-width', '0px').removeClass('emvt_red');
+                $('.emvt_red', emvt.targetPage).removeClass('emvt_red').not('.emvt_blue').css('outline-width', '0px');
                 $(this).addClass('emvt_red').css('outline', '#f00 solid 1px');
+                $(this).on('mouseout', function() {
+                    $('.emvt_blue', emvt.targetPage).css('outline', '#00f solid 1px');
+                });
             }
         });
 
@@ -36,13 +58,14 @@ var emvt = emvt || {};
         emvt.targetPage.on('click', function (e) {
             e.preventDefault();
             emvt.clickedElement = $(e.target);
-            $('.emvt_blue', emvt.targetPage).css('outline-width', '0px');
+            $('.emvt_blue', emvt.targetPage).css('outline-width', '0px').removeClass('emvt_blue');
             emvt.clickedElement.css('outline', '#00f solid 1px').removeClass('emvt_red').addClass('emvt_blue');
-            //emvt.initialiseMenu();
+            emvt.initialiseMenu();
         });
 
         emvt.highlightOn = true;
 
+        // TODO Draggable menu performance is not what it could be
         $('.variate-menu').draggable({
             opacity: 0.7,
             start: function () {
